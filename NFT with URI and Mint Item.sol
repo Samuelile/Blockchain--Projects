545e9d331551b30a4fde98c42c6d127c41f44e83
Oludare S <100304655+Samuelile@users.noreply.github.com>
2022-03-19 06:18:58 -0400

    Create NFT with URI and Mint Item.sol

---
+++ NFT with URI and Mint Item.sol
@@ -0,0 +1,33 @@
+// SPDX-Licence-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721("My NFT Contract" "MNC" {}
+    
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+    
+    function contractURI() public pure returns (string memory) {
+        return "JSON link to contract";
+    }
+    
+    function mintItem(address minter, string memory tokenURI) public onlyOwneer returns (uint256) {
+        
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(minter, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+    
+}
